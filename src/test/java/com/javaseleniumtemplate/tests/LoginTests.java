f45b0b0571a4a0e40f0f1f49255184d5f19af3f9
Lucas Souza <lucassouzaleal96@hotmail.com>
2022-05-05 16:32:16 -0300

    Repositório para armazenar casos de testes que serão utilizados na empresa.

---
+++ src/test/java/com/javaseleniumtemplate/tests/LoginTests.java
@@ -0,0 +1,36 @@
+package com.javaseleniumtemplate.tests;
+
+import com.javaseleniumtemplate.bases.TestBase;
+import com.javaseleniumtemplate.dbsteps.UsuariosDBSteps;
+import com.javaseleniumtemplate.pages.LoginPage;
+import com.javaseleniumtemplate.pages.MainPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+    //Objects
+    LoginPage loginPage;
+    MainPage mainPage;
+
+    //Tests
+    @Test
+    public void efetuarLoginComSucesso(){
+        //Objects instances
+        loginPage = new LoginPage();
+        mainPage = new MainPage();
+
+        //Parameteres
+        String usuario = "lucas.leal@teknisa.com";
+        String senha = "Teknisa1.";
+        String mensagemMainPage = "Inicio";
+
+        //Test
+        loginPage.preenhcerUsuario(usuario);
+        loginPage.preencherSenha(senha);
+        loginPage.clicarEmLogin();
+        loginPage.aceitarMensagemAlerta();
+        //loginPage.aceitarMensagemAlerta2();
+
+        Assert.assertTrue(mainPage.retornaUsernameDasInformacoesDeLogin().contains(mensagemMainPage));
+    }
+}
