761f603a86d9143569cd67c7649a1df27bfdaa46
Lucas Apenas <44064478+LucasMBR@users.noreply.github.com>
2022-05-27 18:01:03 -0300

    Tela de Realtório de Posição de estoque em contrução

--- src/test/java/com/javaseleniumtemplate/pages/RelatorioPosicaoEstoquePage.java
+++ src/test/java/com/javaseleniumtemplate/pages/RelatorioPosicaoEstoquePage.java
@@ -1,7 +1,25 @@
 package com.javaseleniumtemplate.pages;
 
 import com.javaseleniumtemplate.bases.PageBase;
+import org.openqa.selenium.By;
 
 public class RelatorioPosicaoEstoquePage extends PageBase {
+
+    By campoRelatorio = By.xpath("//div/span[text() = 'Posição de Estoque']");
+    By campoUnidade = By.xpath();
+    By campoDataPosicao = By.xpath();
+    By campoAlmoxarifado = By.xpath();
+    By campoTipoDeCusto = By.xpath();
+    By campoNivelTotalizacao = By.xpath();
+    By campoOrdenarPorNome = By.xpath();
+    By campoProdutoInicial = By.xpath();
+    By campoProdutoFinal = By.xpath();
+
+    By btnEmGrid = By.xpath();
+
+    public void preencherRelatorio(){
+
+    }
+
 
 }
